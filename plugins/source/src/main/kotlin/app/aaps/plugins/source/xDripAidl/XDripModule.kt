997f1cb1b218c20ab964c9aceb0055d5eed7dfa6
jerryw02 <132795579+jerryw02@users.noreply.github.com>
2026-01-15 09:56:52 +0800

    Create XDripModule.kt

---
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/xDripAidl/XDripModule.kt
@@ -0,0 +1,18 @@
+package app.aaps.plugins.source.xDripAidl
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(FragmentComponent::class, ServiceComponent::class, ViewModelComponent::class)
+object XDripModule {
+
+    @Provides
+    fun provideXDripPlugin(plugin: XDripPlugin): XDripPlugin {
+        return plugin
+    }
+}
