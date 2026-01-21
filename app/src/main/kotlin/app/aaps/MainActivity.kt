8150ee1dd7c4d0680b7ab84be2f83de6ccaea23d
jerryw02 <132795579+jerryw02@users.noreply.github.com>
2026-01-21 13:38:58 +0800

    Update MainActivity.kt

--- app/src/main/kotlin/app/aaps/MainActivity.kt
+++ app/src/main/kotlin/app/aaps/MainActivity.kt
@@ -2,7 +2,7 @@ package app.aaps
 
 import android.os.Handler
 import android.os.Looper
-import app.aaps.utils.BatteryOptimizationUtil
+//import app.aaps.utils.BatteryOptimizationUtil
 import app.aaps.utils.BatteryWhitelistEnforcer
 
 import android.content.Context
